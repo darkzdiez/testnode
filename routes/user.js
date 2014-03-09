55305b9ebe7da2b6c8f3906a55f5844858da25fe
darkzdiez <you@example.com>
2014-03-09 14:45:54 -0430

    config db

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'root'
+  password : 'dbadmin$yara$2010'
 });
 
 db.query('USE test');
